758e23cea9c84a0b635a97e15a9faa24766eca79
BiodunIjelu <biodunijelu@gmail.com>
2024-01-22 13:52:18 +0100

    adding time blocks into script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,22 @@ $(document).ready(function() {
 
    // Display the current day in the #currentDay element
    $("#currentDay").text("Today is " + currentDay);
-   
+
   });
   
+  // Generate time blocks
+  for (var hour = 9; hour <= 17; hour++) {
+    var timeBlock = $("<div>").addClass("row time-block");
+    var hourDiv = $("<div>").addClass("col-2 hour").text(formatHour(hour));
+    var descriptionInput = $("<input>").addClass("col-8 description");
+    var saveBtn = $("<button>").addClass("col-2 saveBtn").text("Save");
+
+    // Set data-hour attribute to associate with the hour
+    timeBlock.attr("data-hour", hour);
+
+    timeBlock.append(hourDiv, descriptionInput, saveBtn);
+    $(".container").append(timeBlock);
+    
+    // Color-code time blocks
+    updateBlockStyle(timeBlock);
+  }
